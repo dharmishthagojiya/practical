10a105e6f539722c3625a2cc4910ca89164c6aa2
dharmishthagojiya <97020091+dharmishthagojiya@users.noreply.github.com>
2022-02-05 12:46:17 +0530

    Add files via upload

--- custom/Test/__manifest__.py
+++ custom/Test/__manifest__.py
@@ -1,7 +1,8 @@
 {
     'name': 'TestModule',
     'sequence': -100,
-    'depends': ['base','mail'],
-    'data': ['security/ir.model.access.csv', 'views/estate_view.xml'],
+    'depends': ['base', 'mail'],
+    'data': ['security/ir.model.access.csv', 'views/estate_view.xml', 'views/property_type.xml'],
+    'installable': True,
     'application': 'True'
 }
