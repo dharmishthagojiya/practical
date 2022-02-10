cdcebebee67cff85aa4ac0346fcdfa2648f0d356
dharmishthagojiya <97020091+dharmishthagojiya@users.noreply.github.com>
2022-02-10 18:44:17 +0530

    Update property_type.py

--- custom/Test/models/property_type.py
+++ custom/Test/models/property_type.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models,api
 
 
 class TestModel(models.Model):
@@ -9,3 +9,10 @@ class TestModel(models.Model):
     name = fields.Char(string="Property Type", required=True, tracking=True)
     property_ids = fields.One2many('test.model','property_type_id',string="Property")
     sequence = fields.Integer('Sequence', default=1)
+    offer_ids = fields.One2many('estate.property.offer','property_type_id',string="offerids")
+    offer_count = fields.Integer(compute="_compute_offers")
+
+    @api.depends('offer_ids')
+    def _compute_offers(self):
+        self.offer_count = len(self.offer_ids)
+    
