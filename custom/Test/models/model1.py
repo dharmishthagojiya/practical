81a96f6e2f0d975cb6445161886df8c6c36601a7
dharmishthagojiya <97020091+dharmishthagojiya@users.noreply.github.com>
2022-02-10 18:42:46 +0530

    Update model1.py

--- custom/Test/models/model1.py
+++ custom/Test/models/model1.py
@@ -1,39 +1,35 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-
-from odoo import fields, models,api
 from odoo.exceptions import ValidationError
+from odoo import api, fields, models
 
 class TestModel(models.Model):
     _name = "test.model"  #estate_property
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Test Model"
+    _order = "id desc"
 
-    name = fields.Char(required=True,string="Name")
+    name = fields.Char(string="Title", required=True, tracking=True)
     description = fields.Text(string='Description')
     postcode = fields.Char(string='Postcode')
-    date_availability = fields.Date(string='Available From')
-    expected_price = fields.Float(required=True, string='Expected Price')
-    selling_price = fields.Float(string='Selling Price')
+    date_availability = fields.Date(string='Available From', copy=False, default=lambda self: fields.Datetime.add(fields.Datetime.now(),months=3))
+    expected_price = fields.Float(required=True, string='Expected Price', tracking=True)
+    selling_price = fields.Float(string='Selling Price', readonly=True, copy=False, help="Selling price is automatically set when an offer is accepted")
     bedrooms = fields.Integer(default=2, string='Bedrooms')
     living_area = fields.Integer(string='Living Area (sqm)')
     facades = fields.Integer(string='Facades')
     garage = fields.Boolean(string='Garage')
     garden = fields.Boolean(string='Garden')
     garden_area = fields.Integer(string='Garden Area (sqm)')
-    property_type_id = fields.Many2one("estate.property.type", string="Type")
-    garden_orientation = fields.Selection(string='Garden Orientation', selection=[('north', 'North'), ('south', 'South'), ('east', 'East'),
-                                                     ('west', 'West')], default='north')
-    
-    buyer_id = fields.Many2one('res.partner', string='Buyer', index=True, tracking=True, copy=False)
-    seller_id = fields.Many2one('res.users', string='Salesperson', index=True, tracking=True, default=lambda self: self.env.user)
-    tag_ids = fields.Many2many(comodel_name="estate.property.tag", string="Tags")
-    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
-    total_area = fields.Float(compute="_compute_total",string="total area")
-    best_price = fields.Float(compute="_compute_price",string="Best offer")
+    garden_orientation = fields.Selection(string='Garden Orientation', selection=[('north', 'North'), ('south', 'South'), ('east', 'East'),('west', 'West')])
+    state = fields.Selection(string="Status", selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')], default='new', tracking=True)
     active = fields.Boolean(string="Active", default=True)
-    state = fields.Selection(string="State", selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')], default='new', tracking=True)
+    tag_ids = fields.Many2many(comodel_name="estate.property.tag", string="Tags")
+    property_type_id = fields.Many2one("estate.property.type", string="Type")
+    seller_id = fields.Many2one('res.users', string='Salesperson', index=True, tracking=True, default=lambda self: self.env.user)
+    buyer_id = fields.Many2one('res.partner', string='Buyer', tracking=True, copy=False)
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+    total_area = fields.Float(compute="_compute_total", string="Total Area (sqm)")
+    best_price = fields.Integer(compute="_compute_price", string="Best Offer")
+
     _sql_constraints = [
         ('check_expected_price', 'CHECK(expected_price > 0)',
          'The expected price of a property should be greater than 0.'),
@@ -41,10 +37,38 @@ class TestModel(models.Model):
          'The selling price of a property should be positive.')
     ]
 
-   
-    @api.depends("living_area","garden_area")
+    def action_sold(self):
+        self.state = 'sold'
+
+    def action_cancel(self):
+        self.state = 'cancelled'
+
+    @api.model
+    def create(self, vals):
+        if not vals.get('description'):
+            vals['description'] = 'New Property'
+        res = super(TestModel, self).create(vals)
+        return res
+
+    def unlink(self):
+        if self.state not in ('new', 'cancelled'):
+            raise ValidationError("Action Invalid!! Can't delete property if it is not in New or Cancelled State.")
+        for rec in self.offer_ids:
+            rec.unlink()
+        return super(TestModel, self).unlink()
+
+    @api.depends('living_area', 'garden_area')
     def _compute_total(self):
         self.total_area = self.living_area + self.garden_area
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden is True:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        elif self.garden is False:
+            self.garden_area = 0
+            self.garden_orientation = ''
 
     @api.depends("offer_ids.partner_id")
     def _compute_price(self):
@@ -53,50 +77,14 @@ class TestModel(models.Model):
         else:
             self.best_price = 0
 
-    @api.constrains('name')
-    def check_name(self):
-        for record in self:
-             name = self.env['test.model'].search([('name', "=",record.name),('id','!=',record.id)])
-             if name:
-                raise ValidationError( ("Name  %s Already exists" % record.name))
+    def check_offer(self, price):
+        # current = self.env['test.model'].browse(self._context.get('active_id')).offer_ids
+        if price <= self.best_price:
+            raise ValidationError("New offer price must be greater than present offers.")
+        elif self.state == 'new':
+            self.state = 'offer_received'
+        return True
 
-    @api.constrains('name', 'description')
-    def _check_description(self):
-        for record in self:
-             if record.name == record.description:
-                  raise ValidationError("Fields name and description must be different")
-
-    @api.onchange("garden")
-    def _onchange_garden(self):
-        if self.garden == True:
-            self.garden_area = 10
-            self.garden_orientation = 'north'
-
-        elif self.garden is False:
-            self.garden_area = 0
-            self.garden_orientation = ''
-
-    @api.constrains('selling_price', 'expected_price')
-    def _check_percent(self):
-        for term_line in self:
-            if term_line.expected_price == 'percent' and (term_line.selling_price < 0.0 or term_line.selling_price > 90.0):
-                raise ValidationError(_('Percentages on the Payment Terms lines must be  0 to 90.'))
-
-    def action_received(self):
-        self.state = 'offer_received'
-
-    def action_accepted(self):
-        self.state = 'offer_accepted'
-
-    def action_sold(self):
-        self.state = 'sold'
-
-    def action_cancel(self):
-        self.state = 'cancelled'
-
-    def action_new(self):
-        self.state = 'new'
- 
     @api.constrains('selling_price', 'expected_price')
     def _check_selling_price(self):
         for rec in self:
@@ -106,17 +94,12 @@ class TestModel(models.Model):
                     rec.selling_price = 0
                     raise ValidationError("The selling price should not be less than 90% of expected price.")
 
-
-
-    '''def refuse_offer(self, price):
+    def refuse_offer(self, price):
+        print("hello")
         # current = self.env['estate.property.offer'].browse(self._context.get('active_id')).offer_ids
-        for rec in self.offer_ids:
+        for rec in self.id.offer_ids:
             if rec.price == price:
                 pass
             rec.action_reject()
         return True
-'''
-
-    
-
 
